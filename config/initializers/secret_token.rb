6491c35a0af15e829bf5f96de775c850a8783275
Adrienne <adrienne.peirce@gmail.com>
2012-05-28 15:19:59 -0500

    added at start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskyonrails::Application.config.secret_token = 'a5fe06abfbfd46e2fe99ee8517a950ffbbbb356664def195d67b22f22fbe6014b0d66c36d0256306f61d1a78059af0d648e0b3fdf0442a255caffb55bf2de445'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Taskyonrails::Application.config.session_store :cookie_store, key: '_taskyonrails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Taskyonrails::Application.config.session_store :active_record_store
